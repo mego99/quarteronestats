ca6a20e909338f3d3a04f2e63ea847f0f421b0d4
Meg <megunaokawa@gmail.com>
2017-10-09 01:19:01 +0900

    created first scatterplot... might change data source

---
+++ docs/charts.js
@@ -0,0 +1,43 @@
+let firstScatter = {};
+
+firstScatter.svg = d3.select('#first-scatter')
+    .attr('width',850)
+    .attr('height',620);
+
+d3.json('../node_app/data.json',
+(error, data) => {
+  if (error) return console.error(error);
+  console.log(data);
+
+  firstScatter.x = d3.scaleLinear()
+    .domain(d3.extent(data,(d)=>{return d.admit}))
+    .range([50,800]);
+
+  firstScatter.y = d3.scaleLinear()
+    .domain(d3.extent(data,(d)=>{return d.earnings}))
+    .range([600,0]);
+
+  firstScatter.xAxis = d3.axisBottom(firstScatter.x);
+
+  firstScatter.yAxis = d3.axisLeft(firstScatter.y);
+
+  gX = firstScatter.svg.append('g')
+    .call(firstScatter.xAxis)
+    .attr('transform','translate(0,600)');
+
+  gY = firstScatter.svg.append('g')
+    .call(firstScatter.yAxis)
+    .attr('transform','translate(50,0)');
+
+  gData = firstScatter.svg.append('g')
+    .selectAll('circle')
+        .data(data)
+      .enter()
+        .append('circle')
+        .attr('transform',function(d) {
+          let x = firstScatter.x(d.admit);
+          let y = firstScatter.y(d.earnings);
+          return "translate(" + x + ',' + y + ")"
+        })
+        .attr('r',3);
+})
--- node_app/index.js
+++ node_app/index.js
@@ -1,3 +1,4 @@
+"use strict";
 const jf = require('jsonfile');
 const h = require('https');
 
@@ -6,7 +7,7 @@ const url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=3h
   + "school.operating=1&"
   + "_sort=2013.cost.attendance.academic_year:desc&"
   + "_per_page=100&"
-  + "fields=school.name,2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings,2013.cost.attendance.academic_year";
+  + "fields=school.name,2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings,2013.cost.attendance.academic_year,2013.admissions.admission_rate.overall";
 
 h.get(url, res => {
   res.setEncoding('utf8');
@@ -16,11 +17,30 @@ h.get(url, res => {
   });
   res.on('end',(key,val) => {
     body = JSON.parse(body);
+    body = body.results;
+
     jf.writeFile(file, body,
-      {replacer: ['results','school.name','2013.cost.attendance.academic_year','2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings']},
       function(err) {
         if (err) console.error(err);
       }
-    )
+    );
+
+    jf.readFile('data.json',function(err,data) {
+      for (let i = 0; i<100; i++) {
+        data[i]['school'] = data[i]['school.name'];
+        delete data[i]['school.name'];
+        data[i]['cost'] = data[i]['2013.cost.attendance.academic_year'];
+        delete data[i]['2013.cost.attendance.academic_year'];
+        data[i]['earnings'] = data[i]['2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings'];
+        delete data[i]['2013.earnings.10_yrs_after_entry.working_not_enrolled.mean_earnings'];
+        data[i]['admit'] = data[i]['2013.admissions.admission_rate.overall'];
+        delete data[i]['2013.admissions.admission_rate.overall'];
+      }
+      jf.writeFile(file, data,
+        function(err) {
+          if (err) console.error(err);
+        }
+      );
+    })
   })
 });
