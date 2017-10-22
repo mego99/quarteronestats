17a36c6bc7a65a808da8b99276d868e598817526
Meg <megunaokawa@gmail.com>
2017-10-22 14:09:55 +0900

    minor style change

--- docs/charts.js
+++ docs/charts.js
@@ -1,8 +1,11 @@
 let firstScatter = {};
 
+var w = ((Math.max(document.documentElement.clientWidth, window.innerWidth || 0)) * .6);
+var h = 500;
+
 firstScatter.svg = d3.select('#first-scatter')
-    .attr('width',850)
-    .attr('height',620);
+    .attr('width',w+50)
+    .attr('height',h+20);
 
 d3.json('node_app/data.json',
 (error, data) => {
@@ -11,19 +14,20 @@ d3.json('node_app/data.json',
 
   firstScatter.x = d3.scaleLinear()
     .domain(d3.extent(data,(d)=>{return d.act}))
-    .range([50,800]);
+    .range([50,w]);
 
   firstScatter.y = d3.scaleLinear()
     .domain(d3.extent(data,(d)=>{return d.earnings}))
-    .range([600,0]);
+    .range([h,0]);
 
   firstScatter.xAxis = d3.axisBottom(firstScatter.x);
 
   firstScatter.yAxis = d3.axisLeft(firstScatter.y);
 
+
   gX = firstScatter.svg.append('g')
     .call(firstScatter.xAxis)
-    .attr('transform','translate(0,600)');
+    .attr('transform','translate(0,' + h + ')');
 
   gY = firstScatter.svg.append('g')
     .call(firstScatter.yAxis)
