82ba32e27812d143ddbff7f71c54ffea97926908
Meg <megunaokawa@gmail.com>
2017-10-22 13:39:07 +0900

    new images

--- py/clean.py
+++ py/clean.py
@@ -6,8 +6,8 @@ import math
 
 
 sc = pd.read_pickle('data.pkl')
-sc.columns = ['index','admitrate','satscores','mediandebt','cost','earnings','retention','schoolname']
-
+print(sc.columns)
+sc.columns = ['index','admitrate','satscores','mediandebt','cost','earnings','retention','schoolname','ownership']
 sc.dropna()
 
 sc['costsqrt'] = sc['cost'].apply(lambda x:
@@ -25,10 +25,10 @@ model = model.fit()
 print(model.summary())
 
 r = math.sqrt(model.rsquared)
-sx = sc['cost'].std()
-sy = sc['earnings'].std()
-xbar = sc['cost'].mean()
-ybar = sc['earnings'].mean()
+sx = sc['costlog'].std()
+sy = sc['earningslog'].std()
+xbar = sc['costlog'].mean()
+ybar = sc['earningslog'].mean()
 b1 = r * (sy / sx)
 b0 = ybar - (b1 * xbar)
 
@@ -36,9 +36,11 @@ b0 = ybar - (b1 * xbar)
 def regressionLine(x):
     return b0 + (b1 * x)
 
-plt.scatter(sc.cost,sc.earnings,1,c='#4148e8')
-plt.plot(sc.cost, regressionLine(sc.cost), c='#00243d', aa=True)
-plt.xlabel('Cost of Attendance ($)', size='smaller')
-plt.ylabel('Earnings, 10 Years After Entry into Institution ($)', size='smaller')
+colors = {1:'#ec5237',2:'#5346e7',3:'#43d1cd'}
+
+plt.scatter(sc.costlog,sc.earningslog,1,c=sc['ownership'].apply(lambda x: colors[x]))
+# plt.plot(sc.costlog, regressionLine(sc.costlog), c='#079799', aa=True)
+plt.xlabel('Log of Cost of Attendance ($)', size='smaller')
+plt.ylabel('Log of Earnings, 10 Years After Entry into Institution ($)', size='smaller')
 plt.title('Cost vs Earnings in US Higher Ed Institutions', size='smaller')
-plt.savefig('original-line.png', dpi=150, bbox_inches='tight')
+plt.savefig('ownership-grouped.png', dpi=150, bbox_inches='tight')
