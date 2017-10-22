7be53076ce67b60fad51fc97f3ddc3154ab55376
Meg <megunaokawa@gmail.com>
2017-10-22 12:52:13 +0900

    added regression calculator to script

--- py/clean.py
+++ py/clean.py
@@ -6,22 +6,39 @@ import math
 
 
 sc = pd.read_pickle('data.pkl')
-# print(sc.columns)
 sc.columns = ['index','admitrate','satscores','mediandebt','cost','earnings','retention','schoolname']
 
 sc.dropna()
 
-sc['earningslog'] = sc['earnings'].apply(lambda x:
-    math.log(x,10)
+sc['costsqrt'] = sc['cost'].apply(lambda x:
+    math.sqrt(x)
 )
-print(sc.head(1))
+sc['costlog'] = sc['cost'].apply(lambda x:
+    math.log(x)
+)
+sc['earningslog'] = sc['earnings'].apply(lambda x:
+    math.log(x)
+)
 
-# print(sc.info)
-print(sc.columns)
-plt.scatter(sc.cost,sc.earningslog,1)
-# plt.show()
-plt.savefig('foo.png', bbox_inches='tight')
-
-model = sm.OLS(sc.cost,sc.earnings,missing = 'drop')
+model = sm.OLS(sc.costlog,sc.earningslog,missing='drop')
 model = model.fit()
 print(model.summary())
+
+r = math.sqrt(model.rsquared)
+sx = sc['cost'].std()
+sy = sc['earnings'].std()
+xbar = sc['cost'].mean()
+ybar = sc['earnings'].mean()
+b1 = r * (sy / sx)
+b0 = ybar - (b1 * xbar)
+
+
+def regressionLine(x):
+    return b0 + (b1 * x)
+
+plt.scatter(sc.cost,sc.earnings,1,c='#4148e8')
+plt.plot(sc.cost, regressionLine(sc.cost), c='#00243d', aa=True)
+plt.xlabel('Cost of Attendance ($)', size='smaller')
+plt.ylabel('Earnings, 10 Years After Entry into Institution ($)', size='smaller')
+plt.title('Cost vs Earnings in US Higher Ed Institutions', size='smaller')
+plt.savefig('original-line.png', dpi=150, bbox_inches='tight')
